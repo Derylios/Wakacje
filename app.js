d2f63e9d873a616fa43986afd8845ccd8f22df0f
Derylios <114022129+Derylios@users.noreply.github.com>
2024-08-20 22:28:24 +0200

    Update app.js
    Dodanie opcji filtrowanie 'Wroclaw'

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const config = {
       type: 'checkbox',
       title: 'Wycieczki: ',
       columnHeader: 'nazwa', // nazwa kolumny w CSV 
-      listItems: ['Na codzien', 'Krakow', 'Solina', 'Praga'],//dodać nazwy wycieczki
+      listItems: ['Na codzien', 'Krakow', 'Solina', 'Praga', 'Wroclaw'],//dodać nazwy wycieczki
     }, // ListItems - Case sensitive - must match spreadsheet entry; This will take up to six inputs but is best used with a maximum of three;
   ],
   
